f9f99ad6dffa92b156a456f847de19b819cded4e
dhruvam123 <dhruvambhalodiya2002@gmail.com>
2024-02-24 14:59:25 +0530

    successfully9

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://Dhruvam123.github.io/Nutrition.github.io",
   optimizeDeps: {
     include: ['react-bootstrap'],
   },
