b4d6574d5aea909e2d25f43a296c9f045fcfa277
dhruvam123 <dhruvambhalodiya2002@gmail.com>
2024-02-24 21:02:10 +0530

    success

--- backend/App.js
+++ backend/App.js
@@ -516,7 +516,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT,'192.168.63.183', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
